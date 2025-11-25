6d20ed5d7e22792d8ef68493b5b3e48d712bc6cf
JJesus Abarca González <jesus.abarca.g@gmail.com>
2025-11-25 15:35:56 -0600

    --no-edit

--- dev/test/bootstrap.php
+++ dev/test/bootstrap.php
@@ -8,7 +8,7 @@ $options = require __DIR__ . '/../config/doctrine.local.php';
 $cacheDir = __DIR__ . '/../data/DoctrineORMModule';
 global $entityManager;
 global $gqlClient;
-$app_port = getenv('PDSSLIB_APP_PORT') ? getenv('PDSSLIB_APP_PORT') : '8000';
+$app_port = getenv('PDSSLIB_APP_PORT') ? getenv('PDSSLIB_APP_PORT') : '80';
 
 $entityManager = EntityManagerFactory::createInstance($options, $cacheDir, true, '');
-$gqlClient = new GQLClient("http://localhost:{$app_port}/index.php/api");
+$gqlClient = new GQLClient("http://localhost/index.php/api");
