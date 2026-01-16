31e69632c101755674f950ceec167bf4018cecf2
JJesus Abarca González <jesus.abarca.g@gmail.com>
2026-01-16 13:36:11 -0600

    - Se quito la clase DateTimeImmutableType - Se quitaron imports incecesarios en AppModule

--- dev/modules/AppModule/src/AppModule.php
+++ dev/modules/AppModule/src/AppModule.php
@@ -2,25 +2,16 @@
 
 namespace AppModule;
 
-use AppModule\Entities\Account;
-use AppModule\Entities\Comment;
-use AppModule\Entities\Post;
 use AppModule\Entities\User;
 use AppModule\Graphql\ResolversAccount;
 use AppModule\Graphql\ResolversComment;
 use AppModule\Graphql\ResolversPost;
 use AppModule\Graphql\ResolversUser;
-use AppModule\Graphql\TypeAccountConnection;
-use AppModule\Graphql\TypeCommentConnection;
-use AppModule\Graphql\TypePostConnection;
-use AppModule\Graphql\TypeUserConnection;
 use DateTime;
 use GPDCore\Graphql\GPDFieldResolveFactory;
-use GPDCore\Graphql\Types\DateType;
 use GPDCore\Library\AbstractModule;
 use GPDCore\Library\IContextService;
 use GPDCore\Library\ProxyUtilities;
-use GraphQL\Type\Definition\Type;
 
 class AppModule extends AbstractModule
 {
@@ -84,44 +75,7 @@ class AppModule extends AbstractModule
     public function getQueryFields(): array
     {
         return [];
-        // $serviceManager = $this->context->getServiceManager();
-        // $userConnectionType = $serviceManager->get(TypeUserConnection::NAME);
-        // $postConnectionType = $serviceManager->get(TypePostConnection::NAME);
-        // $accountConnectionType = $serviceManager->get(TypeAccountConnection::NAME);
-        // $commentConnectionType = $serviceManager->get(TypeCommentConnection::NAME);
-        // return [
-        //     'echo' =>  [
-        //         'type' => Type::nonNull(Type::string()),
-        //         'args' => [
-        //             'message' => Type::nonNull(Type::string())
-        //         ],
-
-        //         'resolve' => function ($root, $args) {
-        //             return $args["message"];
-        //         }
-        //     ],
-        //     'datetime' => [
-        //         'type' => Type::nonNull($this->context->getServiceManager()->get(DateTime::class)),
-        //         'resolve' => function ($root, $args) {
-        //             return new DateTime();
-        //         }
-        //     ],
-        //     'date' => [
-        //         'type' => Type::nonNull($this->context->getServiceManager()->get(DateType::class)),
-        //         'resolve' => function ($root, $args) {
-        //             return new DateTime();
-        //         }
-        //     ],
-        //     'userConnection' => GPDFieldResolveFactory::buildFieldConnection($this->context, $userConnectionType, User::class),
-        //     'user' => GPDFieldResolveFactory::buildFieldItem($this->context, User::class),
-        //     'postConnection' => GPDFieldResolveFactory::buildFieldConnection($this->context, $postConnectionType, Post::class),
-        //     'post' => GPDFieldResolveFactory::buildFieldItem($this->context, Post::class),
-        //     'accountConnection' => GPDFieldResolveFactory::buildFieldConnection($this->context, $accountConnectionType, Account::class),
-        //     'account' => GPDFieldResolveFactory::buildFieldItem($this->context, Account::class),
-        //     'commentConnection' => GPDFieldResolveFactory::buildFieldConnection($this->context, $commentConnectionType, Comment::class),
-        //     'comment' => GPDFieldResolveFactory::buildFieldItem($this->context, Comment::class),
-
-        // ];
+   
     }
 
     /**
@@ -130,20 +84,6 @@ class AppModule extends AbstractModule
     public function getMutationFields(): array
     {
         return [];
-        // return [
-        //     'createUser' => GPDFieldResolveFactory::buildFieldCreate($this->context, User::class),
-        //     'updateUser' => GPDFieldResolveFactory::buildFieldUpdate($this->context, User::class),
-        //     'deleteUser' => GPDFieldResolveFactory::buildFieldDelete($this->context, User::class),
-        //     'createAccount' => GPDFieldResolveFactory::buildFieldCreate($this->context, Account::class),
-        //     'updateAccount' => GPDFieldResolveFactory::buildFieldUpdate($this->context, Account::class),
-        //     'deleteAccount' => GPDFieldResolveFactory::buildFieldDelete($this->context, Account::class),
-        //     'createPost' => GPDFieldResolveFactory::buildFieldCreate($this->context, Post::class),
-        //     'updatePost' => GPDFieldResolveFactory::buildFieldUpdate($this->context, Post::class),
-        //     'deletePost' => GPDFieldResolveFactory::buildFieldDelete($this->context, Post::class),
-        //     'createComment' => GPDFieldResolveFactory::buildFieldCreate($this->context, Comment::class),
-        //     'updateComment' => GPDFieldResolveFactory::buildFieldUpdate($this->context, Comment::class),
-        //     'deleteComment' => GPDFieldResolveFactory::buildFieldDelete($this->context, Comment::class),
-
-        // ];
+      
     }
 }
